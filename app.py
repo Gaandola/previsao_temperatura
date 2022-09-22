62b3054c2dafacad6b19d00e1e4068653809572a
Vinicius Saviello <viniciussaviello@hotmail.com>
2022-09-21 22:15:54 -0300

    Adicionado o botão de buscar

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests,json
 
 
 app=Flask(__name__)
 app.config['DEBUG'] = True
 api="44bfe54347b8593634153cbfaaaddea4"
-lista=[]
+
+
 @app.route ("/")
-def index():
-    local=["São Paulo","Rio de janeiro","Salvador","Belo Horizonte","Curitiba","Rio Branco"]
-    for a in local:
-        url= f"http://api.openweathermap.org/data/2.5/weather?q={a}&units=metric&appid={api}"
-        r = requests.get(url.format(local)).json()
-        
-        
-        dados = {  
-            'cidade' :a ,
+def form():
+    return render_template("form.html"),200
+
+
+@app.route("/temperatura_local", methods= ["GET","POST"])
+def temperatura_local():
+    local = request.form["nome"]
+    url= f"http://api.openweathermap.org/data/2.5/weather?q={local}&units=metric&appid={api}"
+    r = requests.get(url.format(local)).json()
+    dados = {  
+            'cidade' :local ,
             'temperatura': r['main']['temp'],
             'icone':r['weather'][0]['icon'],
             }
-        print (dados)
-        lista.append(dados)
-    #print(lista)
-    return render_template('previsao_tempo.html',lista=lista,l=local),200
+    print (dados)   
     
-
-
+    return render_template('previsao_tempo.html',dados=dados),200
 
 
 app.run()
